f710608e488877b815700ce68c686bdc93997dce
MikeJDev <ukmjanes@gmail.com>
2020-10-19 20:27:09 -0400

    working on layout components

---
+++ src/components/homeScreen/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './index.css'
+console.log('styles:', styles)
+
+export default function HomeScreen() {
+  return (
+    <div className='container'>
+      hello
+    </div>
+  )
+}
