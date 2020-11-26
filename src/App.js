574eeab57d71c77d2c28995f7d3537bfcd4bf405
MikeJDev <ukmjanes@gmail.com>
2020-11-26 15:42:50 -0500

    adding react router, currently working but nav bar isnt displaying when switching routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
+import MenuContainer from './components/Menu/index'
+import Rooms from './components/Rooms/index'
 
 export default function App() {
 
 
   return (
     <div>
-      hello friends
+      <Router>
+        <div>
+          {/* <Link to="/">Home</Link> */}
+          {/* <Link to="/rooms">Rooms</Link> */}
+          <Switch>
+            <Route path="/rooms">
+              <Rooms />
+            </Route>
+            <Route exact path="/">
+              <MenuContainer />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   )
 }
---
+++ src/components/Menu/index.js
@@ -0,0 +1,158 @@
+import React from 'react'
+import '../Menu/index.css'
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
+import IconButton from '@material-ui/core/IconButton';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import HomeIcon from '@material-ui/icons/Home';
+import MeetingRoomIcon from '@material-ui/icons/MeetingRoom';
+import PhotoIcon from '@material-ui/icons/Photo';
+import FindInPageIcon from '@material-ui/icons/FindInPage';
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Home from '../Home/index'
+import {
+  Link,
+} from "react-router-dom";
+
+const drawerWidth = 180;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  drawer: {
+    [theme.breakpoints.up('sm')]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
+  appBar: {
+    [theme.breakpoints.up('sm')]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+    },
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
+  },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+}));
+
+function MenuContainer(props) {
+  const { window } = props;
+  const classes = useStyles();
+  const theme = useTheme();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <div>
+      <div className={classes.toolbar} />
+      <Divider />
+      <List>
+        <ListItem button key='Home'>
+          <ListItemIcon><HomeIcon /></ListItemIcon>
+          <ListItemText primary='Home' />
+        </ListItem>
+        <Link to='/rooms'>
+          <ListItem button key='Rooms'>
+            <ListItemIcon><MeetingRoomIcon /></ListItemIcon>
+            <ListItemText primary='Rooms' />
+          </ListItem>
+        </Link>
+        <ListItem button key='Pictures'>
+          <ListItemIcon><PhotoIcon /></ListItemIcon>
+          <ListItemText primary='Pictures' />
+        </ListItem>
+        <ListItem button key='Docs'>
+          <ListItemIcon><FindInPageIcon /></ListItemIcon>
+          <ListItemText primary='Docs' />
+        </ListItem>
+      </List>
+    </div>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Management tool
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <nav className={classes.drawer} aria-label="mailbox folders">
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Hidden smUp implementation="css">
+          <Drawer
+            container={container}
+            variant="temporary"
+            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation="css">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            variant="permanent"
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+      </nav>
+      <div className='container'>
+        <Home></Home>
+      </div>
+
+    </div>
+  );
+}
+
+export default MenuContainer
