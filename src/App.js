d9117e76fa4fa1e3ad0fb85c00f051ae3dbf481e
MikeJDev <ukmjanes@gmail.com>
2020-11-26 14:27:22 -0500

    restarting with new components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react'
-import HomeLayout from './components/home/index'
-import HomeScreen from './components/homeScreen/index'
-
 
 export default function App() {
+
+
   return (
     <div>
-      <HomeLayout></HomeLayout>
-      {/* <HomeScreen></HomeScreen> */}
+      hello
     </div>
   )
 }
--- src/components/rooms/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function rooms() {
-  return (
-    <div>
-      this is rooms
-    </div>
-  )
-}
